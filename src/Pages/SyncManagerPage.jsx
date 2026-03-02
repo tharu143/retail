0e65d8db0cca3028ec3b20b337e22bd76a9e7502
apple <apple@apples-iMac.local>
2026-03-02 11:26:44 +0530

    feat: Add SyncManagerPage and push to repository

---
+++ src/Pages/SyncManagerPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Components/Nav/NavBar'
+import SyncManager from '../Components/Admin/SyncManager'
+
+function SyncManagerPage() {
+    return (
+        <>
+            <NavBar />
+            <SyncManager />
+        </>
+    )
+}
+
+export default SyncManagerPage
